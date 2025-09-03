f76a895d26f866c474c5b4de670ae1dadd0e4245
qoqoqo2 <louiezdad@gmail.com>
2025-09-02 19:11:28 -0700

    update 4

---
+++ .config/nvim/init.lua
@@ -0,0 +1,9 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.api.nvim_set_hl(0, "Normal",      { bg = "NONE", ctermbg = "NONE" })
+vim.api.nvim_set_hl(0, "NormalNC",    { bg = "NONE", ctermbg = "NONE" })
+vim.api.nvim_set_hl(0, "NonText",     { fg = "#6ca1f3", bg = "NONE" })
+vim.api.nvim_set_hl(0, "SpecialKey",  { fg = "#7cd47b", bg = "NONE" })
+vim.api.nvim_set_hl(0, "LineNr",      { fg = "#f0f3f6" })
+
--- .config/nvim/init.vim
+++
@@ -1,8 +0,0 @@
-set number
-set relativenumber
-
-hi Normal guibg=NONE ctermbg=NONE
-hi NormalNC guibg=NONE ctermbg=NONE
-hi NonText guifg=#6ca1f3 guibg=NONE
-hi SpecialKey guifg=#7cd47b guibg=NONE
-hi LineNr guifg=#f0f3f6
